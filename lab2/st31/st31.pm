a654663776f03c68999588446ccda074f621a44b
podkolzinasm <podkolzin.asm1504@gubkin.ru>
2015-11-08 21:24:28 +0300

    lab 2
    lab 2

--- lab2/st31/st31.pm
+++ lab2/st31/st31.pm
@@ -1,127 +1,114 @@
-﻿#!wperl.exe
+#!wperl.exe
 package ST31;
-
+use 5.010;
 use strict;
 use warnings;
-use CGI qw(param);
+use CGI qw(:standard);
+use CGI::Carp qw(fatalsToBrowser);
+use CGI;
+
 
 sub st31
+
 {
-	my %DbmFileHash = (); my @list = (); my $index = 0;
-	my @Menu = (\&add, \&delete, \&edit, \&show, \&save, \&load, \&show_form);
+my ($q, $global) = @_;
+my @list = (); 
+my %hash = ();
 
-	$Menu[5]->();
-
-	if(param("name")&&param("club")){
-		$Menu[0]->();
-		$Menu[4]->();
-	}elsif(param("edit_name")&&param("club")){
-		$Menu[2]->(param("number")-1);
-		$Menu[4]->();
-	}elsif(param("delete")){
-		$Menu[1]->(param("delete")-1);
-		$Menu[4]->();
-	}
-
-	$Menu[3]->();
-
-
-	sub add{
-		my $href;
-		$href = {"name", param("name"), "club", param("club")};
-		push(@list, $href);
+sub del{
+	if(@_){
+		splice(@list,$_[0]-1,1);
+		save();
 		return 1;
-	}
+		}
+}
 
-	sub edit{
-		${$list[$_[0]]}{"name"} = param("edit_name");
-		${$list[$_[0]]}{"club"} = param("club");
+sub save
+{dbmopen(%hash, "dbmFile", 0666) ;
+
+	my $f; %hash = ();
+for(my $i = 0; $i < (scalar @list ); $i++)
+{$f = join("::",${$list[$i]}{"name"}, ${$list[$i]}{"club"});
+		  $hash{"rec".$i} = $f;}
+		dbmclose(%hash);
 		return 1;
-	}
+}	
 
-	sub delete{
-		splice(@list,$_[0],1);
-		return 1;
-	}
+sub load{
+my $name,my $club,my $record;
+@list = (); %hash=();
+dbmopen(%hash, "dbmFile", 0666) ;
+while((my $key, my $value) = each(%hash)){
+($name, $club) = split(/::/,$value);
+$record = {	"name",$name,"club",$club};
+push(@list, $record);}
+	dbmclose(%hash);
+		return 1;}	
 
-	sub show{
-		print <<"		END";
-Content-Type:  text/html
-
-		<HTML>
-			<HEAD>
-				<META charset="windows-1251">
-				<TITLE>Market</TITLE>
-			</HEAD>
-			<BODY>
-			<h1>TransferMarket</h1>
-			<hr>
-		END
-		for(my $i = 0; $i < ($#list + 1); $i++){
-			$index = $i+1;
-			if(param("edit")==$index){
-				$Menu[6]->("Edit player", "edit_name",$index,
-								${$list[$i]}{"name"},
-								${$list[$i]}{"club"}
-								); 
+sub show{
+my $index = 0;
+print header(-type=>'text/html',  -charset=>'windows-1251');
+		
+		print	"<Center><h1>TransferMarket</h1></Center><p><hr size = '5'>";
+		
+		for(my $i = 0; $i < (scalar @list ); $i++){
+			 $index = $i+1;
+			if(param("Edit")==$index){show_form("Edit player",
+			$index,
+			$list[$i]->{"name"},
+			$list[$i]->{"club"});
 			}else{
 				print <<"				END";			
-					<p><b>PLAYER $index:</b>
-					<p><i>Name: ${$list[$i]}{"name"}
-					<p>club: ${$list[$i]}{"club"}</i><p>
-					<input type="button" value="Edit" onClick="location.href='?edit=$index';">
-					<input type="button" value="Delete" onClick="location.href='?delete=$index';">
-					<a href='#add';">Add player</a>
+					<p><b>Player $index:</b>
+					<p><i>Name: $list[$i]->{"name"}
+					<p>Club: $list[$i]->{"club"}</i><p>
+					<a href="?Edit=$index&student=$global->{student}">Edit player</a>
+					<a href="?Del=$index&student=$global->{student}">Delete player</a>
+					
 				END
 			}
-		}
-		$Menu[6]->("Add New Player", "name", $index+1, "name", "club");
-		print <<"		END";	
-			</BODY>
-		</HTML>
-		END
 	}
-
-	sub save{
-		dbmopen(%DbmFileHash, "dbfile", 0666) || die "can't open DBM file!\n";
-		my $s; %DbmFileHash = ();
-		for(my $i = 0; $i < ($#list + 1); $i++){
-			$s = join("::",${$list[$i]}{"name"},
-						   ${$list[$i]}{"club"});
-			$DbmFileHash{"el".$i} = $s;
-		}
-		dbmclose(%DbmFileHash);
-		return 1;
-	}
-
-	sub load{
-		my ($name,$club,$href);
-		dbmopen(%DbmFileHash, "dbfile", 0666) || die "can't open DBM file!\n";
-		while((my $key, my $value) = each(%DbmFileHash)){
-			($name,$club) = split(/::/,$value);
-			$href = {"name", $name,"club", $club};
-			push(@list, $href);
-		}
-		dbmclose(%DbmFileHash);
-		return 1;
-	}
-
-	 sub show_form{
+	show_form("Add new player",	$index+1);
+	print "<a href=\"$global->{selfurl}\">Start page</a>";
+}
+	
+  sub show_form{
 		print <<"		END";
-			<a name="add"></a>
 			<form>
-				<hr><p><b>$_[0]</b>
-				<i><input name="number" value=$_[2] size=4  readonly>
-				<p>Enter name: <input name=$_[1] value=$_[3]>
-				<p>Enter club: <input name="club" value=$_[4]></i>
-				<p><input type="submit" value="Consent"></i>
-				<p><input type="button" value="Start page" OnClick="location.href='lab2.pl';">
-			</form><hr>
+				<hr ><p><b>$_[0]</b>
+				<i><input type=hidden name="student" value=$global->{student}>
+				<i><input type = "hidden" name="id" value=$_[1] readonly>
+				<p>Enter name: <input name="name" value=$_[2]>
+				<p>Enter club: <input name="club" value=$_[3]></i>
+				<p><input type="submit" value="Apply"></i>
+				
+			</form><hr ><p><b>$_[0]</b>
 		END
 		return 1;
 	 }
-}
-
+	
+	
+	sub add_edit_rec{
+		if (@_){
+			$list[$_[0]-1]->{"name"} = param("name");
+			$list[$_[0]-1]->{"club"} = param("club");
+			save();
+			return 1;
+		}	
+	}
+	
+	
+	
+	load(); 	
+		add_edit_rec(param("id"));
+		del(param("Del"));
+	
+	show();
+	
+	
+	}
 1;
 
+	
+	
 	
